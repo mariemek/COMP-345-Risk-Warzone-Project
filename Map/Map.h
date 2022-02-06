3718ad5ed12f1fa54d12d1149a2beefa4909e41c
Michael <osujim2012@gmail.com>
2022-02-06 18:20:47 -0500

    Add Map, Territory and Continent classes

--- Map/Map.h
+++ Map/Map.h
@@ -1,153 +1,38 @@
-using namespace std;
-#include <iostream>
+#ifndef MAP_H
+#define MAP_H
 #include <vector>
-#include <list>
-#include <iterator>
-class Player { //temporary declaration
-private:
-	string name;
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+
+class Continent;
+class Territory;
+
+class Continent {
 public:
-	Player();
-	Player(string);
-	~Player(); //destructor
+	string name;
+	vector<Territory*> territories;
 
-
-	string inline getName() {
-		return name;
-	} 
-	//get the name 
-	void inline setName(string tName) {
-		name = tName;
-	} //mutator
+	Continent(string&);
 };
-
 
 class Territory { //declaration of the territory class
-private:
-	string territoryN, continent; //name of the territory and continent
-	Player* owner; // pointer to the owner of this territory
-	int nbOfArmy; // the number of army in the territory
-
 public:
-	Territory();
-	Territory(string, string, Player*, int); 
+	string name;
+	Continent* continent; //name of the territory and continent
+//	Player* owner; // pointer to the owner of this territory
+	int nbOfArmy; // the number of army in the territory
+	vector<Territory*> adjacentTerritories; // adjacentTerritories 
+
+	Territory(string&, Continent*);
 	~Territory();//destructor
 
-	//Accessor method
-	string inline getTerritoryN() {
-		return territoryN;
-	}
-	string inline getContinent() {
-		return continent;
-	}
-	inline Player* getOwner() {
-		return owner;
-	}
-	int inline getArmy() {
-		return nbOfArmy;
-	}
-
-	//Mutator Method
-	void inline setTerritoryN(string tName) {
-		territoryN = tName;
-	}
-	void inline setContinent(string cName) {
-		continent = cName;
-	}
-	void inline setOwner(Player* player) {
-		owner = player;
-	}
-	void inline setArmy(int armies) {
-		nbOfArmy = armies;
-	}
 };
 
-//For the graph we are doing an adjency list structure
-// It will be a vector of vertices
-//We will create a vertex class which contains a pointer to the territory that represents it as well as an id and a list of edges that connects to other vertices
-// Then we will create an edge class which will link two vertices together, it will contain only the id of the territory it points to
-//The Map class represents the graph class that will add vertices and edges together 
-
-
-class Edge { //declaration of the edge class
-
-private:
-	int idToNext;
-public:
-
-	Edge();
-	Edge(int);
-	~Edge(); // destructor
-
-	int inline getIdToNext() {
-		return idToNext;
-	} //accessor
-
-	void inline setIdToNext(int tId) {
-		idToNext = tId;
-	} // mutator
-
-
-};
-
-class Vertex { //declaration of the vertex class
-private:
-	int id;
-	Territory* territory;
-	
-
-
-public: 
-	list<Edge> edgeL;
-	Vertex();
-	Vertex(int, Territory*);
-	~Vertex(); //destructor
-
-	//Accessor
-	int inline getId() {
-		return id;
-	}
-
-	inline Territory* getTerritory() {
-		return territory;
-	}
-
-	inline list<Edge> getEdgeList() {
-		return edgeL;
-	}
-
-	//Mutator
-
-	void inline setId(int id) {
-		this->id = id;
-	}
-	void inline setTerritory(Territory* t) {
-		territory = t;
-	}
-	
-	void printEdgeList();
-		
-	
-
-
-};
 class Map { //declaration of the map
-private:
-	 
-	
-
 public:
-	vector<Vertex> vertices;
-	vector<Vertex> getVertices() {
-		return vertices;
-	}
-
-	void addVertex(Vertex);
-	bool vertexAlreadyExist(int vertexId);
-	void addEdge(int idFromV, int idToV);
-	bool edgeAlreadyExist(int, int);
-	Vertex getVertexById(int);
-	void printGraph();
-	
-	
+	unordered_map<string, Territory*> territories;
+	unordered_map<string, Continent*> continents;
 };
+#endif
