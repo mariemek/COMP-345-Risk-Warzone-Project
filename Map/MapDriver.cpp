5447b43f2fb53ab4276e507fb55e82b43d2fed01
Michael <osujim2012@gmail.com>
2022-02-07 16:27:54 -0500

    Revert "Revert "Add driver logic to try multiple map files.""
    This reverts commit fbd9bfe048a1d6a525bb8893f04c3fb01354c6d5.

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -6,7 +6,25 @@ using std::endl;
 
 int main()
 {
-    string filename("solar.map");
-    Map& map = MapLoader::createMap(filename);
-    cout << map.validate();
+    vector<string> filenames ({ "solar.map" });
+    vector<Map*> validmaps;
+
+    for (string filename : filenames) {
+        try {
+            Map& map = MapLoader::createMap(filename);
+            if (map.validate()) {
+                cout << "The map found in \"" << filename << "\" is valid." << endl;
+                validmaps.push_back(&map);
+            }
+            else throw "Invalid Map";
+        }
+        catch (const char* txtException) {
+            cout << "Exception: " << txtException << endl;
+            cout << "Rejected file " << filename << ". It is an invalid file.";
+        }
+        catch (...) {
+            cout << "Rejected file " << filename << ". It is an invalid file.";
+        }
+    }
+    
 } 
