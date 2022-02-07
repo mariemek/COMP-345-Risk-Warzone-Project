b1cd65c9d6afdd39495bd0cd251564a30cf5da37
Michael <osujim2012@gmail.com>
2022-02-06 21:15:47 -0500

    Add driver logic to try multiple map files.

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 int main()
 {
-    string filename("solar.map");
-    Map& map = MapLoader::createMap(filename);
-    cout << map.validate();
+    vector<string> filenames ({ "solar.map" });
+    vector<Map*> validmaps;
+
+    for (string filename : filenames) {
+        try {
+            Map& map = MapLoader::createMap(filename);
+            if (map.validate()) {
+                cout << "The map found in \"" << filename << "\" is valid." << endl;
+                validmaps.push_back(&map);
+            }
+            else throw "Invalid Map";
+        }
+        catch (const char* txtException) {
+            cout << "Exception: " << txtException << endl;
+            cout << "Rejected file " << filename << ". It is an invalid file.";
+        }
+        catch (...) {
+            cout << "Rejected file " << filename << ". It is an invalid file.";
+        }
+    }
+    
 } 
